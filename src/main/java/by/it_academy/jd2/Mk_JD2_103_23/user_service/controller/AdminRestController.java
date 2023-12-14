0b18cc731e4ee085782dd76e036967933ac5f72e
L1BER2Y <xerdan.9@mail.ru>
2023-12-14 22:13:09 +0300

    AdminRestController ready

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_103_23/user_service/controller/AdminRestController.java
@@ -0,0 +1,74 @@
+package by.it_academy.jd2.Mk_JD2_103_23.user_service.controller;
+
+import by.it_academy.jd2.Mk_JD2_103_23.user_service.core.dto.PageDTO;
+import by.it_academy.jd2.Mk_JD2_103_23.user_service.core.dto.UserCreateDTO;
+import by.it_academy.jd2.Mk_JD2_103_23.user_service.core.dto.UserDTO;
+import by.it_academy.jd2.Mk_JD2_103_23.user_service.dao.entity.UserEntity;
+import by.it_academy.jd2.Mk_JD2_103_23.user_service.service.api.IUserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class AdminRestController {
+    private final IUserService service;
+    private final ModelMapper modelMapper;
+
+    public AdminRestController(IUserService service, ModelMapper modelMapper) {
+        this.service = service;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<String> createUser(@RequestBody UserCreateDTO user) {
+        UserEntity userEntity = convertToEntity(user);
+        this.service.createUser(userEntity);
+        return new ResponseEntity<>("Пользователь добавлен", HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<PageDTO> getPage(@RequestParam(defaultValue =  "0") Integer page,
+                                           @RequestParam(defaultValue = "20") Integer size
+    ) {
+        PageDTO pageDTO = new PageDTO(page, size);
+        Page<UserEntity> page1 = this.service.getPage(pageDTO);
+        PageDTO page2 = new PageDTO(page, size, page1.getTotalPages(),page1.getTotalElements(),
+                                    page1.isFirst(), page1.getSize(),
+                                    page1.isLast(), page1);
+        return ResponseEntity.ok(page2);
+    }
+
+    @GetMapping("/{uuid}")
+    @ResponseBody
+    public UserDTO getUser(@PathVariable("uuid") UUID id) {
+        return convertToDto(this.service.findById(id));
+    }
+
+    @PutMapping("/{uuid}/dt_update/{dt_update}")
+    public void updateUser(@PathVariable("uuid") UUID uuid,
+                           @PathVariable("dt_update") LocalDateTime dt_update,
+                           @RequestBody UserCreateDTO userCreateDTO) {
+        if(!Objects.equals(uuid, this.service.findById(uuid).getId()) && !Objects.equals(dt_update, this.service.findById(uuid).getDtUpdate())) {
+            throw new IllegalArgumentException("Запрос содержит некорректные данные. Измените запрос и отправьте его ещё раз");
+        }
+        UserEntity userEntity = convertToEntity(userCreateDTO);
+        userEntity.setDtUpdate(dt_update);
+        this.service.updateUser(userEntity);
+    }
+
+    private UserDTO convertToDto(UserEntity entity) {
+        return modelMapper.map(entity, UserDTO.class);
+    }
+
+    private UserEntity convertToEntity(UserCreateDTO userDTO) {
+        return modelMapper.map(userDTO, UserEntity.class);
+    }
+}
--- src/main/java/by/it_academy/jd2/Mk_JD2_103_23/user_service/controller/UserRestController.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_103_23/user_service/controller/UserRestController.java
@@ -1,17 +1,13 @@
 package by.it_academy.jd2.Mk_JD2_103_23.user_service.controller;
 
-import by.it_academy.jd2.Mk_JD2_103_23.user_service.core.dto.PageDTO;
-import by.it_academy.jd2.Mk_JD2_103_23.user_service.core.dto.UserCreateDTO;
 import by.it_academy.jd2.Mk_JD2_103_23.user_service.core.dto.UserDTO;
+import by.it_academy.jd2.Mk_JD2_103_23.user_service.core.dto.UserRegDTO;
 import by.it_academy.jd2.Mk_JD2_103_23.user_service.dao.entity.UserEntity;
 import by.it_academy.jd2.Mk_JD2_103_23.user_service.service.api.IUserService;
 import org.modelmapper.ModelMapper;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -24,44 +20,18 @@ public class UserRestController {
         this.modelMapper = modelMapper;
     }
 
-    @PostMapping
+    @PostMapping("/registration")
     @ResponseBody
-    public ResponseEntity<String> createUser(@RequestBody UserCreateDTO user) {
-        UserEntity userEntity = convertToEntity(user);
-        this.service.createUser(userEntity);
-        return new ResponseEntity<>("Пользователь добавлен", HttpStatus.CREATED);
+    public ResponseEntity<String> registration(@RequestBody UserRegDTO userRegDTO) {
+        UserEntity userEntity = convertToEntity(userRegDTO);
+        return new ResponseEntity<>("Пользователь зарегистрирован", HttpStatus.CREATED);
     }
-
-    @GetMapping()
-    @ResponseBody
-    public ResponseEntity<Page<PageDTO>> getPage(@RequestParam(defaultValue =  "0") Integer page,
-                                                 @RequestParam(defaultValue = "20") Integer size
-    ) {
-        PageDTO pageDTO = new PageDTO(page, size);
-//        Page<PageDTO> servicePage = this.service.getPage(pageDTO);
-        return ResponseEntity.ok()
-                .body();
-    }
-
-    @GetMapping("/{uuid}")
-    @ResponseBody
-    public UserDTO getUser(@RequestParam("uuid") UUID id) {
-        return convertToDto(this.service.getById(id));
-    }
-
-//    @PutMapping("/{uuid}/dt_update/{dt_update}")
-//    public void updateUser(@RequestParam("uuid") UUID uuid,
-//                           @RequestParam("dt_update") LocalDateTime dt_update,
-//                           @RequestBody UserDTO userDTO) {
-//        UserEntity userEntity = convertToEntity(userDTO);
-//        this.service.updateUser(userEntity);
-//    }
 
     private UserDTO convertToDto(UserEntity entity) {
         return modelMapper.map(entity, UserDTO.class);
     }
 
-    private UserEntity convertToEntity(UserCreateDTO userDTO) {
+    private UserEntity convertToEntity(UserRegDTO userDTO) {
         return modelMapper.map(userDTO, UserEntity.class);
     }
 }
