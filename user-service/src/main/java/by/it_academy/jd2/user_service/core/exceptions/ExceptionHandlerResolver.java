8d5ef9ee11f6e0c719fa121c39a102c91015b700
L1BER2Y <xerdan.9@mail.ru>
2024-02-18 20:28:30 +0300

    ExceptionHandlerResolver response type fix

--- user-service/src/main/java/by/it_academy/jd2/user_service/core/exceptions/ExceptionHandlerResolver.java
+++ user-service/src/main/java/by/it_academy/jd2/user_service/core/exceptions/ExceptionHandlerResolver.java
@@ -3,6 +3,7 @@ package by.it_academy.jd2.user_service.core.exceptions;
 import by.it_academy.jd2.user_service.core.exceptions.body.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,34 +23,31 @@ public class ExceptionHandlerResolver {
                 "Запрос некорректен. Попробуйте заново.");
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ValidationException.class)
-    public String validationError(ValidationException exception) {
+    public ResponseEntity<ErrorResponse> validationError(ValidationException exception) {
         log.error(exception.getMessage());
-        return exception.getMessage();
+        return new ResponseEntity<>(new ErrorResponse("error", exception.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedException.class)
-    public String unauthorized(UnauthorizedException exception) {
+    public ErrorResponse unauthorized(UnauthorizedException exception) {
         log.error(exception.getMessage());
-        return exception.getMessage();
+        return new ErrorResponse("error", exception.getMessage());
     }
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(AccessDeniedException.class)
-    public String accessDenied(AccessDeniedException exception) {
+    public ErrorResponse accessDenied(AccessDeniedException exception) {
         log.error(exception.getMessage());
-        return exception.getMessage();
+        return new ErrorResponse("error", exception.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(InternalServerErrorException.class)
     public ErrorResponse serverError(InternalServerErrorException exception) {
         log.error(exception.getMessage());
-        return new ErrorResponse(
-                "error",
-                "Сервер не смог корректно обработать запрос. Пожалуйста обратитесь к администратору");
+        return new ErrorResponse("error", exception.getMessage());
     }
 
 }
--- user-service/src/main/java/by/it_academy/jd2/user_service/core/exceptions/VerificationException.java
+++ user-service/src/main/java/by/it_academy/jd2/user_service/core/exceptions/VerificationException.java
@@ -1,6 +1,6 @@
 package by.it_academy.jd2.user_service.core.exceptions;
 
-public class VerificationException extends Exception{
+public class VerificationException extends RuntimeException{
 
     public VerificationException(String message) {
         super(message);
